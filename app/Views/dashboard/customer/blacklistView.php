5893822da3270ca5de892267721d880b1a5ec19f
Tharindu Gihan <wtgihan@gmail.com>
2021-03-24 23:40:05 +0530

    customers and room search process complete

--- app/Views/dashboard/customer/blacklistView.php
+++ app/Views/dashboard/customer/blacklistView.php
@@ -246,7 +246,20 @@
                                         <?php  echo $row['check_out_date'];?>
                                     </div>
                                 </td>
-                                <td><?php echo $row['payment_method'];?></td>
+                                <td>
+                                    <?php 
+                                        if($row['payment_method'] == "ONLINEONLINE") {
+                                            echo "ONLINE";
+                                        }
+                                        elseif($row['payment_method'] == "CASHONLINE") {
+                                            echo "CASH";
+                                        }
+                                        else {
+                                            echo $row['payment_method'];
+                                        }
+                                    ?>
+                                    
+                                </td>
                                 <td>
                                     <?php  if($row['check_in_status'] == NULL AND $row['check_in_status'] == NULL ){?>
                                     <div class="out">    
